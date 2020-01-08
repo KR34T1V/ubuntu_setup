052bb1dff530ecb9885d1fdd73090a49c3df3d12
Cornelis Terblanche <m4dm0nk3y.za@gmail.com>
2020-01-08 11:44:34 +0200

    vscode

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
 # Author : Cornelis Terblanche
-# Copyright (c) GNU v3:
+# Copyright (c) GNU v3
 # Script follows here:
-# basic-packages.sh
+# vscode-setup.sh
+wget https://packages.microsoft.com/keys/microsoft.asc -O - | sudo apt-key add
+VSCODE_KEY="https://packages.microsoft.com/keys/microsoft.asc"
+VSCODE_REPO="deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 
-INSTALL_REPOS=""
+INSTALL_KEYS="$VSCODE_KEY"
 
-INSTALL_PACKAGES="build-essential git curl apt-transport-https wget"
+INSTALL_REPOS="$VSCODE_REPO"
+
+INSTALL_PACKAGES="software-properties-common code"
 
 install () {
 	echo "Repositories to be installed: $INSTALL_REPOS"
 	echo "Packages to be installed: $INSTALL_PACKAGES"
-
+	
+	wget -q $INSTALL_KEYS -O- | sudo apt-key add
 	sudo add-apt-repository --yes $INSTALL_REPOS
 	sudo apt update
 	sudo apt upgrade --yes
@@ -36,7 +42,7 @@ remove() {
 	sudo apt remove --yes $REMOVE_PACKAGES
 	sudo apt purge --yes $REMOVE_PACKAGES
 	sudo apt update
-        sudo apt autoremove --yes
+    sudo apt autoremove --yes
 	echo "\nSetup Complete"
 
 }
